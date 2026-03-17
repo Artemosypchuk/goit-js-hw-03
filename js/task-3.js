80d01f814b57d6dbb10ea97043cb425c7f3cffc2
Artem <tyoska111@ukr.net>
2026-03-17 18:07:41 +0200

    3-rd HW-JS

--- js/task-3.js
+++ js/task-3.js
@@ -1,13 +1,9 @@
-function checkForSpam(message){
-    const txt = message.toLowerCase();
-    return txt.includes("sale") || txt.includes("spam");
+function filterArray(numbers, value){
+    const filterredNumbers = [];
+    for (const number of numbers) {
+        if (number > value) {
+            filterredNumbers.push(number);
+        }
+    }
+    return filterredNumbers;
 }
-
-
-console.log(checkForSpam("Latest technology news")); // false
-console.log(checkForSpam("JavaScript weekly newsletter")); // false
-console.log(checkForSpam("Get best sale offers now!")); // true
-console.log(checkForSpam("Amazing SalE, only tonight!")); // true
-console.log(checkForSpam("Trust me, this is not a spam message")); // true
-console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!")); // true
-console.log(checkForSpam("[SPAM] How to earn fast money?")); // true
